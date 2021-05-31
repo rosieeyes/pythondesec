69da45e79a9365e4b910e032a664d6c18606c0d9
Rosie Sweet <rose@rjasweet.com>
2021-05-31 21:05:56 +0100

    poetry.lock: Added dev-dependencies; re-versioned on Python 3.9.4
    Added both of pylint and black to the dev-dependencies.
    Re-versioned all packages slightly due to pyenv update to Python
    3.9.4; this update was done to get explicit encoding setting in
    logging.basicConfig()
    
    pythondesec/desec.py:
    Added barebones class and logging.

--- pythondesec/desec.py
+++ pythondesec/desec.py
@@ -1,3 +1,15 @@
 import httpx
+import logging
 
-class 
+logging.basicConfig(
+    filename="pythondesec.log",
+    encoding='utf-8',
+    format="%(asctime)s %(message)s",
+    level=logging.DEBUG
+)
+
+class Desec:
+    def __init__(self):
+        self.httpclient = httpx.Client()
+        
+        logging.info("Desec Client initialised")
